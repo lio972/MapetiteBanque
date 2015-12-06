fe3a3fa19fb53c0251f154f79fdf6337f1804414
lio <lio@lio-OptiPlex-390>
2015-12-06 03:38:29 +0100

    nouveau test

---
+++ MapetitBanque/src/org/$6tmic/consulting/principal/Adresse.java
@@ -0,0 +1,41 @@
+package org.$6tmic.consulting.principal;
+
+public class Adresse {
+	private String nomDeRue;
+	private String complementAdresse;
+	private String codePostal;
+	private String nomDeVille;
+	
+	private String numeroDeRue;
+	public String getNumeroDeRue() {
+		return numeroDeRue;
+	}
+	public void setNumeroDeRue(String numeroDeRue) {
+		this.numeroDeRue = numeroDeRue;
+	}
+	public String getNomDeRue() {
+		return nomDeRue;
+	}
+	public void setNomDeRue(String nomDeRue) {
+		this.nomDeRue = nomDeRue;
+	}
+	public String getComplementAdresse() {
+		return complementAdresse;
+	}
+	public void setComplementAdresse(String complementAdresse) {
+		this.complementAdresse = complementAdresse;
+	}
+	public String getCodePostal() {
+		return codePostal;
+	}
+	public void setCodePostal(String codePostal) {
+		this.codePostal = codePostal;
+	}
+	public String getNomDeVille() {
+		return nomDeVille;
+	}
+	public void setNomDeVille(String nomDeVille) {
+		this.nomDeVille = nomDeVille;
+	}
+	
+}
---
+++ MapetitBanque/src/org/$6tmic/consulting/principal/Banque.java
@@ -0,0 +1,19 @@
+package org.$6tmic.consulting.principal;
+
+public class Banque {
+	
+	private String bNom;
+	private Adresse bAdresse;
+	private String bIdentifiant;
+	
+	public Banque(){
+		
+	}
+	
+	public Banque(String identifiant){
+		bIdentifiant = identifiant;
+		
+	}
+	
+
+}
